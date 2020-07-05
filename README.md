9f1311405509fc479df198bcbe1f826aa33aa91f
Cruzo007 <csoni.chirag@gmail.com>
2020-07-05 11:29:43 +0530

    Touchups & Comments

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # NSE-Nifty500-Data
- A tool for extracting Nifty/NSE data
+ *  A tool for extracting Nifty/NSE data using the NSEpy library.
+    Docs: https://nsepy.xyz/
+ 
+ *  The repo is fine-tuned for serving the data required for https://github.com/Cruzo007/Nifty-Returns-Vizualizer,
+    but should be easily modifiable to fit individual needs/requirements
--- driver.py
+++ driver.py
@@ -2,18 +2,20 @@ from nsepy import get_history
 from datetime import date
 import pandas as pd
 
-
+# List of Stocks/Symbols you are interested in.
 df_ticks = pd.read_csv("Nifty500_list.csv")
 
 for ticker_ind in df_ticks.index:
     print(df_ticks['Ticker'][ticker_ind])
     stock_df = get_history(symbol=df_ticks['Ticker'][ticker_ind], start=date(2020, 6, 1), end=date(2020, 6, 30))
-    # Clear Excess Columns
+
+    # Clear Excess Columns, one might have different requirements. Read the NSEpy's docs.
     stock_df.drop(stock_df.iloc[:, 1:5], inplace=True, axis=1)
     stock_df.drop(stock_df.iloc[:, 4:10], inplace=True, axis=1)
     stock_df.drop(['Low', 'Last'], inplace=True, axis=1)
 
     # Compute Returns for each day, taking base cost as the Closing price of first day
+    # + Adding fields from the csv/list if any.
     Returns = []
     Sectors = []
     MarketCaps = []
@@ -32,4 +34,6 @@ for ticker_ind in df_ticks.index:
     stock_df['Sector'] = Sectors
     stock_df.rename(columns={'Symbol': 'Ticker'}, inplace=True)
 
+    # Outputting with headers(Column names) as false, otherwise
+    # we'll have row entries with column names for each Stock/Symbol
     stock_df.to_csv('out.csv', header=False, index=True, date_format="%d-%m-%Y", mode="a")
